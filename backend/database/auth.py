e6280a812ed565486c581daff72769bf3d21359a
jenia petrusenko <medvijenia@icloud.com>
2025-10-15 02:18:49 +0300

    auth progression

--- backend/database/auth.py
+++ backend/database/auth.py
@@ -1,5 +1,4 @@
-import requests
-from typing import Optional
+from supabase import create_client
 from backend.utils.logs import Logfire
 from backend.database.manager import DatabaseManager
 from backend.settings import Config
@@ -10,16 +9,23 @@ log = Logfire(name='auth-functions')
 
 class Auth(DatabaseManager):
 
-    url = f"{Config.SUPABASE_URL}/auth/v1/admin/users"
+    def create_user(self, email: str, use_otp: bool = True) -> dict:
+        supabase = create_client(supabase_url=Config.SUPABASE_URL, supabase_key=Config.SUPABASE_ROLE_KEY)
 
-    def create_user(self, email: str) -> None:
-        response = self.db.auth.sign_in_with_otp(
-            {
-                "email": email,
-                "options": {
-                    "email_redirect_to": "http://localhost:5000/welcome",
-                },
-            }
-        )
-        log.fire.info(f'{response.model_dump()}')
-        return response.model_dump()
+        if use_otp:
+            self.db.auth.sign_in_with_otp(
+                {
+                    "email": email,
+                    "options": {"email_redirect_to": "http://localhost:5000/welcome"},
+                }
+            )
+            log.fire.info(f"OTP sent to {email}")
+            return {"status": "otp_sent"}
+
+        response = supabase.auth.admin.create_user({
+            "email": email,
+            "password": "default123",
+            "email_confirm": True,
+        })
+        log.fire.info(f"User created: {response}")
+        return response.model_dump_json()
